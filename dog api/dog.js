c1e0b0387264142a89e5791d147955537421b3cd
brahmbhatt maithili07 <8505barotmaithili@gmail.com>
2024-12-25 21:44:40 +0530

    Add files via upload

---
+++ dog api/dog.js
@@ -0,0 +1,42 @@
+
+
+
+let mainSection = document.getElementById("dog");
+
+function fetchdata() {
+    fetch('https://www.freetestapi.com/api/v1/dogs')
+        .then((res) => res.json())
+        .then((data) => {
+            card(data);
+        })
+        .catch((err) => console.log("Error fetching data:", err));
+}
+fetchdata();
+
+function card(data) {
+    const store = data.map((ele) =>
+        adddata(ele.id, ele.image, ele.name, ele.size)
+    );
+    mainSection.innerHTML = store.join('');
+}
+
+function adddata(id, image, name,size) {
+    return `
+    <div class="card" data-id="${id}">
+        <div class="card-img">
+            <img src="${image}" width="200px" alt="${name}">
+        </div>
+        <div class="card-body">
+            <h4 class="card-name">${name}</h4>
+          
+            <p class="card-price">size: ${size}</p>
+        </div>
+    </div>
+    `;
+}
+
+
+
+
+
+
